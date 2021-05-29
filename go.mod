98088a83b4a76560df4463b8904108dad32ad648
adesupraptolaia <ades@alterra.id>
2021-05-29 17:55:40 +0700

    upgrade module, using v2 version

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/adesupraptolaia/golang-modules-app
 
 go 1.14
 
-require github.com/adesupraptolaia/golang-modules-package v1.0.1
+require github.com/adesupraptolaia/golang-modules-package/v2 v2.0.0
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	belajargolangmodules "github.com/adesupraptolaia/golang-modules-package"
+	belajargolangmodules "github.com/adesupraptolaia/golang-modules-package/v2"
 )
 
 func main() {
-	fmt.Println(belajargolangmodules.SayHello())
+	fmt.Println(belajargolangmodules.SayHello("Ade"))
 }
