cf9ceef07f7c515b9849ac9c6892090b8a772fa6
adesupraptolaia <ades@alterra.id>
2021-05-29 17:44:39 +0700

    test using package v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/adesupraptolaia/golang-modules-app
+
+go 1.14
+
+require github.com/adesupraptolaia/golang-modules-package v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	belajargolangmodules "github.com/adesupraptolaia/golang-modules-package"
+)
+
+func main() {
+	fmt.Println(belajargolangmodules.SayHello())
+}
